c81ef64ac9265af372fd22dd258c130b65d9c95e
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-08 20:58:52 +0100

    Added logging for development

--- server.js
+++ server.js
@@ -42,6 +42,10 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
+  console.log('REQUEST')
+  console.log(req);
+  console.log('RESPONSE')
+  console.log(res);
   next();
 });
 
