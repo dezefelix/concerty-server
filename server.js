ac961356135c4ac46cd2599fef73adb7eb9ac2a7
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-08 20:41:22 +0100

    Fixed in string format for acces control allow headers

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.set('env', (process.env.ENV || 'development'));
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
