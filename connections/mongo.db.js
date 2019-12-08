381f84d672ada5444fa0c32dd6bb24dba5039ac6
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-08 18:36:38 +0100

    Allow log dbUrl (for dev purposes)

--- connections/mongo.db.js
+++ connections/mongo.db.js
@@ -3,6 +3,9 @@ const config = require('../config/config');
 
 mongoose.Promise = global.Promise;
 
+console.log(config.dbUrl);
+console.log(config.dbUrl);
+console.log(config.dbUrl);
 mongoose.connect(config.dbUrl, { useNewUrlParser: true });
 const connection = mongoose.connection
 .once('open', () => console.log('Connected to Mongo on ' + config.dbUrl))
