e7a9324e0c9a84e4b87996c74f9b7ab055037da4
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-08 20:37:56 +0100

    Fixed typo

--- server.js
+++ server.js
@@ -37,13 +37,9 @@ app.use(expressJWT({
 app.set('port', (process.env.PORT || config.env.webPort));
 app.set('env', (process.env.ENV || 'development'));
 
-console.log(process.env.ALLOW_ORIGIN);
-console.log(process.env.ALLOW_ORIGIN);
-console.log(process.env.ALLOW_ORIGIN);
-
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200' || 'localhost:4200');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
