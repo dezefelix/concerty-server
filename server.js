df1486a91b56f6fcf9b555c66d82e023675c1b04
ALTERNATE\Felix <felixboons@hotmail.com>
2019-12-11 14:49:12 +0100

    Allowing all origins for sure and commented handy info

--- server.js
+++ server.js
@@ -37,15 +37,9 @@ app.use(expressJWT({
 app.set('port', (process.env.PORT || config.env.webPort));
 app.set('env', (process.env.ENV || 'DEVELOPMENT'));
 
-const env = app.get('env');
-console.log(env);
-console.log(env);
-console.log(env);
-
 app.use(function (req, res, next) {
-  console.log(req.headers.origin);
-  console.log(req.headers.origin);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Origin', req.headers.origin); // Allow all origins (handy for development).
+  res.header('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // OPTIONS is required for successful request.
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
